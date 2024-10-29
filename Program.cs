13199ddb17e70e7b1cfcd3a1172dea2ef78affc7
Milos Spasojevic <milos.s.pfc@gmail.com>
2024-10-29 14:14:39 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using ESC_POS_USB_NET.Printer;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Printer printer = new Printer("EPSON TM-T(180dpi_1) Receipt6", "IBM860");
+printer.TestPrinter();
+printer.FullPaperCut();
+printer.PrintDocument();
+
+Console.WriteLine("Done");
